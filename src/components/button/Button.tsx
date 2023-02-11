cdf282e41852b0c0eaf2f8def0dca4e2764ca774
feleks <nashvel@gmail.com>
2023-02-12 00:51:27 +0300

    Реализован спиннер и спиннер-мод для кнопки.

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,16 +1,29 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames';
 import React from 'react';
-
 import './Button.scss';
 
 interface Props {
     value: string;
     submit?: boolean;
+    loading?: boolean;
     className?: string;
 }
 
 export function Button(props: Props) {
-    const { value, submit = false, className } = props;
+    const { value, submit = false, loading = false, className } = props;
 
-    return <input className={classNames('component-button', className)} type={submit ? 'submit' : 'button'} value={value} />;
+    // return <input className={classNames('component-button', className)} type={submit ? 'submit' : 'button'} value={value} />;
+
+    let finalValue: any = value;
+    if (loading) {
+        finalValue = <FontAwesomeIcon className="spinner" icon={faSpinner} />;
+    }
+
+    return (
+        <button type={submit ? 'submit' : 'button'} className={classNames('component-button', { loading }, className)}>
+            {finalValue}
+        </button>
+    );
 }
